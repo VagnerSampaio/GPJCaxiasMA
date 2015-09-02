7112b5be7ffb2629890029eefdf072ff79ee5075
João Vagner de Alencar Sampaio <vagnerd@live.com>
2015-09-01 22:06:03 -0300

    Tentando MySQL
    Desisto!!!!

--- controllers/resultados.js
+++ controllers/resultados.js
@@ -1,4 +1,20 @@
 module.exports = function(app){
+	/*
+	app.getConnection(function(err,connection){
+		connection.query('SELECT * FROM tcc.unidade u, tcc.meta m, tcc.metasunidades mu, tcc.apuracao a'+
+			'where u.idUnidade = mu.Unidade_idUnidade '+
+			'and mu.Meta_idMeta = m.idMeta '+
+			'and m.idMeta = a.Meta_idMeta '+
+			'and u.idUnidade = a.Unidade_idUnidade '+
+			'order by a.dataApuracao;',function(err,rows){
+			if(err){
+				console.log("Erro BD: "+err);
+			} else {
+				
+			}
+		})
+	});
+	*/
 	var resultadosController = {
 		//action: index
 		index: function(req,res){
